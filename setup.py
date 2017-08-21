d88b7797fe5433c840d1dc94b295ee2b86ca53c2
Christian Junker <chjdev@gmail.com>
2017-08-21 16:44:30 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="fl-brain",
-    version="3.0.0",
+    name="brain",
+    version="4.0.0",
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'fl-base',
+        'common',  # fanlens
         'pycld2',
         'nltk',
         'retinasdk',
@@ -24,6 +24,5 @@ setup(
         'google-cloud-translate',
         'mstranslator',
         'python-dateutil',
-    ],
-    package_data={'fl-brain.nltk_data': ['*']}
+    ]
 )
